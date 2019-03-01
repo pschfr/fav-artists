95c977705f9aa85858a7900e628e4de164d62fc4
Paul Schaefer <yeppaul2@gmail.com>
2019-03-01 12:30:11 -0500

    Added lastfmapi dependency for actual API usage

--- api/index.js
+++ api/index.js
@@ -1,8 +1,24 @@
 // Require dependencies
+const LastFM_API = require('lastfmapi');
+const LastFM = new LastFM_API({
+	'api_key': process.env.LAST_FM_KEY,
+	'secret': process.env.LAST_FM_SECRET
+});
+
+// Wrapper function to get top artists from LastFM
+function LastFM_top_artists(username = 'paul_r_schaefer') {
+	LastFM.user.getTopArtists({ user: username }, (err, data) => {
+		if (err) console.error(err)
+		else {
+			// console.log(data['artist']);
+			return data;
+		}
+	});
+}
 
 // Exports to Zero
 module.exports = (request, response) => {
 	response.send({
-		string: "Hello world!"
+		response: LastFM_top_artists()
 	});
 }
--- index.jsx
+++ index.jsx
@@ -5,11 +5,11 @@ export default class extends React.Component {
 	// called by Zero on page load
 	static async getInitialProps(){
 		var json = await fetch("/api/index").then((resp) => resp.json())
-		return {string: json.string}
+		return { response: json.response }
 	}
 
 	// Rendered to the page
 	render() {
-		return <p>Current string is: {this.props.string}</p>
+		return <p>Current data is:<br/><pre>{JSON.stringify(this.props.response)}</pre></p>
 	}
 }
